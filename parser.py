535048ad4d187fd27a4278d2df0027a7dff6eef2
Daniel Shrimpton <44175578+DanielShrimpton@users.noreply.github.com>
2020-03-13 02:57:09 +0000

    Added logging and write grammar to file

--- parser.py
+++ parser.py
@@ -2,11 +2,13 @@
 import math
 import re
 import sys
-from anytree import Node, RenderTree
+from anytree import Node
 from anytree.exporter import UniqueDotExporter
+from datetime import datetime
 
 INPUT_FOLDER = './Inputs/'
 OUTPUT_FOLDER = './Outputs/'
+LOG = './log/log.log'
 tree = {'<S>': Node('<S>')}
 START = tree['<S>']
 
@@ -51,7 +53,7 @@ class Grammar:
             file = INPUT_FOLDER + sys.argv[1]
             self.file_name = sys.argv[1].strip('.txt')
         except IndexError:
-            print("Warning: no file supplied, using default 'example.txt'")
+            log('warning', "Warning: no file supplied, using default 'example.txt'")
             file = INPUT_FOLDER + 'example.txt'
             self.file_name = 'example'
         try:
@@ -70,23 +72,27 @@ class Grammar:
                                       re.findall(r'[ ](\S+)\[(\d+)\]', line)]
                         for pred, size in predicates:
                             if '(' in pred:
-                                sys.stderr.write("NameError: illegal character found in predicate "
-                                                 "%s, the character '(' is not allowed" % pred)
+                                st = "NameError: illegal character found in predicate %s, " \
+                                     "the character '(' is not allowed" % pred
+                                log("error", st)
                                 sys.exit(1)
                             elif ')' in pred:
-                                sys.stderr.write("NameError: illegal character found in predicate "
-                                                 "%s, the character ')' is not allowed" % pred)
+                                st = "NameError: illegal character found in predicate %s, " \
+                                     "the character ')' is not allowed" % pred
+                                log("error", st)
                                 sys.exit(1)
                             elif ':' in pred:
-                                sys.stderr.write("NameError: illegal character found in predicate "
-                                                 "%s, the character ':' is not allowed" % pred)
+                                st = "NameError: illegal character found in predicate %s, " \
+                                     "the character ':' is not allowed" % pred
+                                log("error", st)
                                 sys.exit(1)
                             self.arity[pred] = size
                     elif 'equality:' in line:
                         self.equality = re.findall(r'[ ](\S+)', line)
                         if len(self.equality) != 1:
-                            sys.stderr.write("InputError: incorrect number of equalities supplied, "
-                                             "there needs to be 1")
+                            st = "InputError: incorrect number of equalities supplied, " \
+                                 "there needs to be 1"
+                            log("error", st)
                             sys.exit(2)
                         else:
                             self.equality = self.equality[0]
@@ -97,16 +103,18 @@ class Grammar:
                         self.connectives = [re.escape(x) if '\\' in x else x for x in
                                             re.findall(r'[ ](\S+)', line)]
                         if len(self.connectives) != 5:
-                            sys.stderr.write("InputError: incorrect number of connectives "
-                                             "supplied, there needs to be 5")
+                            st = "InputError: incorrect number of connectives supplied, " \
+                                 "there needs to be 5"
+                            log('error', st)
                             sys.exit(1)
                         self.names += self.connectives
                     elif 'quantifiers:' in line:
                         self.quantifiers = [re.escape(x) if '\\' in x else x for x in
                                             re.findall(r'[ ](\S+)', line)]
                         if len(self.quantifiers) != 2:
-                            sys.stderr.write("InputError: incorrect number of quantifiers "
-                                             "supplied, there needs to be 2")
+                            st = "InputError: incorrect number of quantifiers supplied, " \
+                                 "there needs to be 2"
+                            log("error", st)
                             sys.exit(1)
                         self.names += self.quantifiers
                     elif 'formula:' in line:
@@ -120,44 +128,47 @@ class Grammar:
                                 else:
                                     self.formula.append(x)
         except FileNotFoundError:
-            sys.stderr.write("FileNotFoundError: cannot find file: %s" % file)
+            log("error", "FileNotFoundError: cannot find file: %s" % file)
             sys.exit(-1)
 
         self.predicates = list(self.arity.keys())
         self.names += self.predicates
         names_set = set(self.names)
         if len(names_set) != len(self.names):
-            sys.stderr.write("InputError: no duplicate names allowed")
+            log("error", "InputError: no duplicate names allowed")
             sys.exit(1)
 
         for name in self.names:
             if re.search(r'^<', name):
-                sys.stderr.write("NameError: items cannot begin with '<'")
+                log("error", "NameError: items cannot begin with '<'")
                 sys.exit(1)
             elif re.search(r'\(', name):
-                sys.stderr.write("NameError: items cannot contain the character '('")
+                log("error", "NameError: items cannot contain the character '('")
                 sys.exit(1)
             elif re.search(r':', name):
-                sys.stderr.write("NameError: items cannot contain the character ':'")
+                log("error", "NameError: items cannot contain the character ':'")
                 sys.exit(1)
             elif re.search(r'\)', name):
-                sys.stderr.write("NameError: items cannot contain the character ')'")
+                log("error", "NameError: items cannot contain the character ')'")
                 sys.exit(1)
-            elif re.search(r'=', name):
-                if name != self.equality:
-                    sys.stderr.write("NameError: items cannot contain the character '=' unless it "
-                                     "is equality")
-                    sys.exit(1)
+            # elif re.search(r'=', name):
+            #     if name != self.equality:
+            #         log("error", "NameError: items cannot contain the character '=' unless it is "
+            #                      "equality")
+            #         sys.exit(1)
             elif re.search(r'/', name):
                 if name in self.connectives:
-                    sys.stderr.write("NameError: connectives cannot contain character '/'")
+                    log("error", "NameError: connectives cannot contain character '/'")
                     sys.exit(1)
                 elif name in self.quantifiers:
-                    sys.stderr.write("NameError: quantifiers cannot contain character '/'")
+                    log("error", "NameError: quantifiers cannot contain character '/'")
+                    sys.exit(1)
+                else:
+                    log("error", "NameError: illegal character '/' detected in %s" % name)
                     sys.exit(1)
             elif not re.match(r'^[\w\\_]+$', name):
-                if not ((name == self.equality) and re.match(r'=', name)):
-                    sys.stderr.write("NameError: illegal character detected in %s" % name)
+                if not ((name == self.equality) and re.match(r'^[\w\\_=]+$', name)):
+                    log("error", "NameError: illegal character detected in %s" % name)
                     sys.exit(1)
 
         self.neg = self.connectives.pop(-1)
@@ -208,6 +219,7 @@ class Grammar:
                         }
 
     def print_grammar(self):
+        self.terminals = [x.replace('\\\\', '\\') if '\\\\' in x else x for x in self.terminals]
         print('\nTerminal symbols: ' + '|'.join(self.terminals))
         print('\nNon-Terminal symbols: ' + '|'.join(self.non_terminals))
         print('\nStart symbol: <S>')
@@ -217,7 +229,8 @@ class Grammar:
             t = math.ceil((12 - len(key)) / 4)
             if key == '<S>':
                 t = 2
-            print(key + '\t' * t + '->\t' + '|'.join(self.grammar[key]))
+            print(key + '\t' * t + '->\t' + '|'.join(self.grammar[key]).replace('\\\\', '\\'))
+        log("OK", "Successfully parsed formula")
 
     def add_node(self, tag, parent=0):
         if parent != 0:
@@ -227,6 +240,7 @@ class Grammar:
 
         tree[str(self.ID)] = Node(tag, parent=parent)
         self.ID += 1
+        log("info", "Added node %s" % tag)
 
     def parse(self):
         self.current = 0
@@ -234,8 +248,8 @@ class Grammar:
         if self.current == len(self.formula):
             self.print_grammar()
         else:
-            sys.stderr.write("SyntaxError: Unexpected part of formula: %s" % ' '.join(self.formula[
-                                                                                      self.current:]))
+            log("error", "SyntaxError: Unexpected part of formula: %s" % ' '.join(self.formula[
+                                                                                  self.current:]))
             sys.exit(1)
 
     def _formula(self):
@@ -307,7 +321,7 @@ class Grammar:
             self.current += 1
 
         else:
-            sys.stderr.write("SyntaxError: Unexpected character '%s'" % atom)
+            log("error", "SyntaxError: Unexpected character '%s'" % atom)
             sys.exit(1)
 
     def _quant(self):
@@ -316,8 +330,8 @@ class Grammar:
             self.add_node('<var>')
             self.add_node(atom, 1)
         else:
-            sys.stderr.write("SyntaxError: quantifier not followed by known variable: %s %s" %
-                             (self.formula[self.current - 1], atom))
+            log("error", "SyntaxError: quantifier not followed by known variable: %s %s" % (
+                self.formula[self.current - 1], atom))
             sys.exit(1)
         self.current += 1
 
@@ -326,7 +340,7 @@ class Grammar:
             self.add_node('<constVar>')
             self.add_node(atom, parent=1)
         else:
-            sys.stderr.write("SyntaxError: assignment using unknown constant/variable: %s" % atom)
+            log("error", "SyntaxError: assignment using unknown constant/variable: %s" % atom)
             sys.exit(1)
         self.current += 1
 
@@ -343,8 +357,8 @@ class Grammar:
         if atom == ')':
             self.add_node(')')
         else:
-            sys.stderr.write("SyntaxError, assignment missing closing bracket: %s" %
-                             " ".join(self.formula[self.current - 4:self.current]))
+            log("error", "SyntaxError, assignment missing closing bracket: %s" % " ".join(
+                self.formula[self.current - 4:self.current]))
             sys.exit(1)
         self.current += 1
 
@@ -354,7 +368,7 @@ class Grammar:
         if atom == '(':
             self.add_node('(')
         else:
-            sys.stderr.write("SyntaxError: predicate %s isn't followed by '('." % pred)
+            log("error", "SyntaxError: predicate %s isn't followed by '('." % pred)
             sys.exit(1)
         arity = int(self.arity[pred]) - 1
         self.current += 1
@@ -367,23 +381,22 @@ class Grammar:
                 self.current += 2
                 atom = self.formula[self.current]
             else:
-                sys.stderr.write("Error, syntax error! Predicate %s isn't formatted correctly"
-                                 % pred)
+                log("error", "Error, syntax error! Predicate %s isn't formatted correctly" % pred)
                 sys.exit(1)
             arity -= 1
         if atom in self.variables:
             self.add_node('<var>')
             self.add_node(atom, 1)
         else:
-            sys.stderr.write("Error, syntax error! Predicate %s isn't formatted correctly" % pred)
+            log("error", "Error, syntax error! Predicate %s isn't formatted correctly" % pred)
             sys.exit(1)
         self.current += 1
         atom = self.formula[self.current]
         if atom == ')':
             self.add_node(')')
         else:
-            sys.stderr.write("Error, syntax error! Predicate %s isn't formatted correctly: "
-                             "missing ')'" % pred)
+            log("error", "Error, syntax error! Predicate %s isn't formatted correctly: missing "
+                         "')'" % pred)
             sys.exit(1)
         self.current += 1
 
@@ -399,13 +412,42 @@ class Grammar:
             elif atom in self.connectives:
                 if open_ == self.open:
                     return atom
-        sys.stderr.write("Error, invalid syntax, redundant brackets")
+        log("error", "Error, invalid syntax, redundant brackets")
         sys.exit(1)
 
     def save_tree(self):
         UniqueDotExporter(self.root).to_picture('Outputs/' + self.file_name + '.png')
 
+    def write_grammar(self):
+        with open('Outputs/' + self.file_name + '-grammar.txt.', 'w') as file:
+            file.write('Terminal symbols: %s\n' % '|'.join(self.terminals))
+            file.write('\nNon-Terminal symbols: %s\n' % '|'.join(self.non_terminals))
+            file.write('\nStart symbol: <S>\n')
+            file.write("\nGrammar Production Rules: \n\n")
+
+            for key in self.grammar.keys():
+                t = math.ceil((12 - len(key)) / 4)
+                if key == '<S>':
+                    t = 2
+                file.write(key + '\t' * t + '->\t' + '|'.join(self.grammar[key])
+                           .replace('\\\\', '\\') + '\n')
+
+
+def log(type_, string):
+    if type_ == 'warning':
+        print(string)
+    elif type_ == 'OK':
+        pass
+    elif type_ == 'info':
+        pass
+    else:
+        sys.stderr.write(string + '\n')
+    now = datetime.now().strftime("%H:%M:%S")
+    with open(LOG, 'a') as file:
+        file.write(now + '\t' + type_ + '\t' + string + '\n')
+
 
 thing = Grammar()
 thing.parse()
 thing.save_tree()
+thing.write_grammar()
